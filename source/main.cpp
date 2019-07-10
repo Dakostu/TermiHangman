7bae42be069bab79b71cb9c17e414868fb80445f
DK <dkostuj@gmx.de>
2019-07-10 18:22:34 +0000

    finalized unit tests

--- source/main.cpp
+++ source/main.cpp
@@ -2,40 +2,55 @@
 // Use of this source code is governed by the license that can be
 // found in the LICENSE file.
 
+#include "GrabBag.h"
 #include "hangmanvector.h"
 #include "settostring.h"
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
     
 int main(int argc, char** argv) {        
-	if (argc == 1) {
-		cout << "Need a filename as an argument" << endl;
-		return 1;
-	}
-	
-	int tries = 12;
-	HangmanVector wordToGuess(argv[1]);
+    if (argc == 1) {
+        cout << "Need a filename as an argument" << endl;
+        return 1;
+    }
+    
+    // save words from file into a GrabBag
+    ifstream fin(argv[1]);
+    GrabBag<string> words;
+    string currentWord;
+    while (fin >> currentWord)
+        words.insert(currentWord);
+    if (!words.size()) {
+        cerr << "File is empty." << endl;
+        exit(1);
+    }
+    
+    
+    int tries = 12;
     string response = "";
+    HangmanVector wordToGuess(words.grab());
     
     // This set displays all letters that have been already entered
     set<char> enteredTries;
     
-	while (!wordToGuess.isGuessed()) {
+    while (!wordToGuess.isGuessed()) {
         
         printf("\033c"); // clear terminal screen at every new try        
         
         cout << "*** TERMI-HANGMAN ***" << endl;
         cout << response << endl; 
-		cout << wordToGuess.toString() << endl;
-		cout << "Enter the letter(s) you're guessing.\tRemaining tries: " 
+        cout << wordToGuess.toString() << endl;
+        cout << "Enter the letter(s) you're guessing.\tRemaining tries: " 
         << tries << "\t" << setToString(enteredTries) << endl;
         
         char input;
         cin >> input;
         
                 
-        if (wordToGuess.WordContains(input))
+        if (wordToGuess.wordContains(input))
             response = "Correct!";
         else {
             response = "Ouch!";
@@ -58,8 +73,8 @@ int main(int argc, char** argv) {
             return 0;
         }
         
-	}
-	return 0;
+    }
+    return 0;
 }
 
 
